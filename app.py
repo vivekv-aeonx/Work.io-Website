4f2e592a3baa44cdea565e227948b1f49812acbd
vivekv2412 <vivek.vajha@aeonx.digital>
2024-08-22 11:20:15 +0000

    22/08/24- 4:48pm

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, jsonify
+from database import engine, load_jobs_from_db, load_job_from_db
+
+
+app=Flask(__name__)
+
+@app.route("/")
+def home_page():
+  jobs=load_jobs_from_db()
+  return render_template("homedemo.html", jobs=jobs)
+
+@app.route("/jobs/<id>")
+def show_job(id):
+  job=load_job_from_db(id)
+  return render_template("jobpage.html",job=job)
+
+if __name__=="__main__":
+  app.run(host='0.0.0.0', debug=True)
+  
