b91b74d23a1d3e3df85ad01dbb864b2db0e6ea7d
vivekv2412 <vivek.vajha@aeonx.digital>
2024-08-23 06:45:33 +0000

    add application data to mysql

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from db import engine, load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, request, jsonify
+from db import engine, load_jobs_from_db, load_job_from_db, add_application_to_db
 
 
 app=Flask(__name__)
@@ -14,6 +14,15 @@ def show_job(id):
   job=load_job_from_db(id)
   return render_template("jobpage.html",job=job)
 
+@app.route("/jobs/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data=request.form
+  job= load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template("application_submitted.html",application=data)
+
+
+  
 if __name__=="__main__":
   app.run(host='0.0.0.0', debug=True)
   
